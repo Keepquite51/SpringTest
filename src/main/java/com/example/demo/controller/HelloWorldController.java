a0c874fd199a92799e28a187c39f197ca3366a15
Keepquite51 <1468596754@qq.com>
2019-08-10 09:07:25 +0800

    提交更改

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
+	// ’‚¿Ô «◊¢ Õ
     @RequestMapping("/say.html")
     @ResponseBody
     public String say(){
